012d7523415bbe8a30779919c5179e8177f9b6a9
Mike Gazdich <mgazdich@vt.edu>
2012-04-14 15:10:24 -0400

    Java doc

--- CS2114 Snakes/src/mmv/CS2114/Snakes/SnakeView.java
+++ CS2114 Snakes/src/mmv/CS2114/Snakes/SnakeView.java
@@ -1,5 +1,14 @@
 package mmv.CS2114.Snakes;
-
+/**
+ * // -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author Gazdich
+ *  @version Apr 14, 2012
+ */
 public class SnakeView
 {
     //Whats up Mike
