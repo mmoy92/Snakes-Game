8518e7f4650cb113504a046682b21a037393fa43
Michael Moy <mikedmoy@gmail.com>
2012-04-14 16:10:20 -0400

    Updated comments.

--- CS2114 Snakes/src/mmv/CS2114/Snakes/SnakePart.java
+++ CS2114 Snakes/src/mmv/CS2114/Snakes/SnakePart.java
@@ -1,10 +1,30 @@
 package mmv.CS2114.Snakes;
 
+/**
+ * // -------------------------------------------------------------------------
+ * /** Class for each individual segment of the snake.
+ * 
+ * @author Michael Moy (mmoy92)
+ * @version 2012.04.14
+ */
 public class SnakePart
 {
-    public int x, y;
+    /**
+     * x field.
+     */
+    public int x;
+    /**
+     * y field.
+     */
+    public int y;
 
 
+    // ----------------------------------------------------------
+    /**
+     * Create a new SnakePart object.
+     * @param x -X coordinate of snake part.
+     * @param y -Y coordinate of snake part.
+     */
     public SnakePart(int x, int y)
     {
         this.x = x;
