27a3bea4cf0366f8b66e2771247e12a9ea1c2637
Vincent Ngo <vincentn@vt.edu>
2012-04-14 15:07:12 -0400

    Added some comments

--- CS2114 Snakes/src/mmv/CS2114/Snakes/CS2114SnakesActivity.java
+++ CS2114 Snakes/src/mmv/CS2114/Snakes/CS2114SnakesActivity.java
@@ -3,6 +3,14 @@ package mmv.CS2114.Snakes;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * // -------------------------------------------------------------------------
+/**
+ *  Test test test.
+ * 
+ *  @author vincentngo
+ *  @version Apr 14, 2012
+ */
 public class CS2114SnakesActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
