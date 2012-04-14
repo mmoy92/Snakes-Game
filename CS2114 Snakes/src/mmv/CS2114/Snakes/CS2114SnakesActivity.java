7d9e752df2a4b2da8b4465170eee83c2528684b6
Vincent Ngo <vincentn@vt.edu>
2012-04-14 15:00:57 -0400

    New Project Folder Test

---
+++ CS2114 Snakes/src/mmv/CS2114/Snakes/CS2114SnakesActivity.java
@@ -0,0 +1,13 @@
+package mmv.CS2114.Snakes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CS2114SnakesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
